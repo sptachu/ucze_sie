a9a0b9634dd5b0834b6d9e383a1038c6ab46285d
unknown <stachuptak25@gmail.com>
2026-07-23 11:37:05 +0200

    feat: add date picker and display chosen date using v-calendar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+  import {ref} from 'vue';
+  import { DatePicker } from 'v-calendar';
+  import 'v-calendar/style.css'
+
+  const wybrana_data = ref(new Date());
+
 </script>
 
 <template>
+
+  <div>
+    <h2>Wybierz datę</h2>
+    <DatePicker v-model="wybrana_data"/>
+    <p>Wybrałeś: {{ wybrana_data }}</p>
+  </div>
+
   <header>
     <div class="wrapper">
-      <HelloWorld msg="You did not do it!" />
+      <!--<HelloWorld msg="You did not do it!" />-->
+      no to mam gotowe
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <!--<TheWelcome />-->
+    i nie wiem co
   </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+  line-height: 1.5;   
 }
 
 .logo {
