80cc57b563244778280b705e0598581d58d325ae
unknown <stachuptak25@gmail.com>
2026-07-29 10:42:27 +0200

    refactor: atomize project structure to be compatible with best practices

---
+++ src/features/home/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <MyCalendar v-model="draftDateRange" />
+
+    <MainButton btn-type="mainBtn" @click="handleSaveDateRange">
+      Zatwierdź
+    </MainButton>
+
+    <div class="result-container">
+      <p class="result-text">Wybrałeś:</p>
+      <strong class="result-text">{{ draftDateRange }}</strong>
+    </div>
+    
+  </main>
+</template>
+
+<script setup lang="ts">
+  import MyCalendar from '@/features/home/components/MyCalendar.vue';
+  import MainButton from '@/features/shared/components/MainButton.vue';
+  import useCalendar from './composables/useCalendar';
+  const {draftDateRange, handleSaveDateRange} = useCalendar();
+  
+
+</script>
+
+<style scoped>
+  main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  align-items: center;
+  gap: 2rem; /* Dodajemy odstęp między kalendarzem a napisami pod spodem */
+}
+
+.result-text {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,50 +1,13 @@
-<script setup>
- import { ref } from 'vue';
-  import MyCalendar from '@/components/MyCalendar.vue';
-  import MainButton from '@/components/MainButton.vue';
-  import { useCalendarStore } from '@/stores/calendarStore';
-  const calendarStore = useCalendarStore();
-
-  const draftDateRange = ref({
-    start: new Date(),
-    end: new Date()
-  });
-
-  const handleSave = () => {
-    calendarStore.addDateRange(draftDateRange.value);
-    alert('Daty zostały pomyślnie zapisane w magazynie!');
-  };
-
-</script>
-
 <template>
   <main>
-    <MyCalendar v-model="draftDateRange" />
-
-    <MainButton btn-type="mainBtn" @click="handleSave">
-      Zatwierdź
-    </MainButton>
-
-    <div class="result-container">
-      <p class="result-text">Wybrałeś:</p>
-      <strong class="result-text">{{ draftDateRange }}</strong>
-    </div>
-    
+    <HomePage/>
   </main>
 </template>
 
-<style scoped>
-  main {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 100vh;
-  align-items: center;
-  gap: 2rem; /* Dodajemy odstęp między kalendarzem a napisami pod spodem */
-}
+<script setup>
 
-.result-text {
-  font-family: Arial, Helvetica, sans-serif;
-  text-align: center;
-}
-</style>
+  import HomePage from '@/features/home/HomePage.vue';
+
+</script>
+
+
